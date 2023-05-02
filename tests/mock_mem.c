b01d46e79c5e129026a09c6c3155b552074dbb80
Tomochika Hara <github@thara.dev>
2023-05-03 00:50:40 +0900

    :white_check_mark: Add mock implementation for mem

---
+++ tests/mock_mem.c
@@ -0,0 +1,19 @@
+#include "string.h"
+
+#include "mem.h"
+
+#include "mock_mem.h"
+
+static uint8_t memory[10000];
+
+void mem_init() {
+    memset(memory, 0, sizeof(memory));
+}
+
+uint8_t MEM_MOCKABLE(mem_read)(struct nes *nes, uint16_t addr) {
+    return memory[addr];
+}
+
+void MEM_MOCKABLE(mem_write)(struct nes *nes, uint16_t addr, uint8_t val) {
+    memory[addr] = val;
+}
---
+++ tests/mock_mem.h
@@ -0,0 +1,6 @@
+#ifndef MOCK_MEM_H
+#define MOCK_MEM_H
+
+void mem_init();
+
+#endif // MOCK_MEM_H
