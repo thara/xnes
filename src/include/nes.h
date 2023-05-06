67228606f6b7d1768411e70adc40338a2c8a0270
Tomochika Hara <github@thara.dev>
2023-05-06 22:53:24 +0900

    :muscle: Read/Write to WRAM

--- src/include/nes.h
+++ src/include/nes.h
@@ -1,13 +1,19 @@
 #ifndef NES_H
 #define NES_H
 
+#include "stdint.h"
+
 #include "cpu.h"
 
 typedef struct {
     CPU cpu;
+
+    uint8_t wram[0x0800];
 } NES;
 
 NES* nes_new();
+
+void nes_step(NES* nes);
 
 void nes_release(NES* nes);
 
--- src/nes.c
+++ src/nes.c
@@ -2,6 +2,7 @@
 
 #include "nes.h"
 #include "cpu.h"
+#include "cpu_emulation.h"
 
 NES *nes_new() {
   NES *nes = malloc(sizeof(NES));
@@ -10,6 +11,8 @@ NES *nes_new() {
   }
   return nes;
 }
+
+void nes_step(NES *nes) { cpu_step(nes); }
 
 void nes_release(NES *nes) {
   if (nes != NULL) {
