f169d8e8badfcd03a3931ecb26f0d92dd4732086
Tomochika Hara <github@thara.dev>
2023-06-03 16:22:09 +0900

    :art: make format

--- src/ppu.c
+++ src/ppu.c
@@ -1,21 +1,21 @@
 #include "ppu.h"
 
 void ppu_power_on(PPU *ppu) {
-    // https://www.nesdev.org/wiki/PPU_power_up_state
-    ppu_reset(ppu);
-    ppu->status = 0;
+  // https://www.nesdev.org/wiki/PPU_power_up_state
+  ppu_reset(ppu);
+  ppu->status = 0;
 }
 
 void ppu_reset(PPU *ppu) {
-    ppu->ctrl = 0;
-    ppu->mask = 0;
-    ppu->data = 0;
-    ppu->oam_addr = 0;
+  ppu->ctrl = 0;
+  ppu->mask = 0;
+  ppu->data = 0;
+  ppu->oam_addr = 0;
 
-    ppu->v = 0;
-    ppu->t = 0;
-    ppu->x = 0;
-    ppu->w = false;
+  ppu->v = 0;
+  ppu->t = 0;
+  ppu->x = 0;
+  ppu->w = false;
 }
 
 void ppu_ctrl_set(PPU *ppu, PPUCTRL e, bool v) {
